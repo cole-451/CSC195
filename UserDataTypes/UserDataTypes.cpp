60b510dec17a8f3f2d774b360dfb320d92b16262
cole-451 <coleneely87@gmail.com>
2025-04-02 12:30:26 -0600

    Added Employee class for Payroll Program

--- UserDataTypes/UserDataTypes.cpp
+++ UserDataTypes/UserDataTypes.cpp
@@ -3,5 +3,28 @@
 
 int main()
 {
+    int numOfEmployees;
+    std::cout << "Number of Employees (max 5):";
+    std::cin >> numOfEmployees;
+
+    if (numOfEmployees > 5) {
+        numOfEmployees = 5;
+    }
+
+        Employee employees[5]; //since Employee is an object, we can make an array of said objects.
+
+        for (int i = 0; i < numOfEmployees; i++) {
+            std::cout << "Enter details for Employee number " << (i + 1) << ":\n";
+            employees[i].Read();
+
+        }
+
+        std::cout << "\n Payroll Report \n";
+        for (int i = 0; i < numOfEmployees; i++) {
+            employees[i].Write();
+        }
+        return 0;
+        
     
+
 }
